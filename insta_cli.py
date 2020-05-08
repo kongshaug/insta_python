6a455cebce8205aedda8f7075130023bb5f56d52
Amanda <Manda_Hansen@hotmail.com>
2020-05-08 20:08:53 +0200

    cli service

---
+++ insta_cli.py
@@ -0,0 +1,81 @@
+import argparse
+import pickle
+from insta_bot import instagram_bot
+from neural_network import neural_network
+
+parser = argparse.ArgumentParser(description="")
+
+parser.add_argument(
+    "-ha",
+    "--hashtag",
+    type=str,
+    required=False,
+    help="Enter hashtag for Instagram search",
+)
+
+parser.add_argument(
+    "-n",
+    "--number-of-images",
+    type=int,
+    required=False,
+    help="Enter number of images to download",
+)
+
+parser.add_argument(
+    "-has",
+    "--hashtags",
+    type=str,
+    required=False,
+    help="Enter hashtags for the categories in the neural network",
+)
+
+parser.add_argument(
+    "-t", "--test", action="store_true", required=False, help="Test the neural network"
+)
+
+parser.add_argument(
+    "-l",
+    "--link",
+    type=str,
+    required=False,
+    help="Enter link to the image, you want predicted",
+)
+
+
+def get_data_from_instagram(hashtag, number_of_images):
+    bot = instagram_bot(hashtag)
+    bot.get_images(number_of_images)
+    print("The image has been downloaded to the file {}.csv in /images".format(hashtag))
+
+
+def make_neural_network(hashtags):
+    network = neural_network(hashtags)
+    network.make_model()
+    network.train_network()
+    network.pickle_network()
+    print("The neural network is trained, pickled and ready for use")
+
+
+def test_network():
+    pickle_in = open("network.pickle", "rb")
+    network = pickle.load(pickle_in)
+    pickle_in.close()
+    network.test_network()
+
+
+def predict(link):
+    pickle_in = open("network.pickle", "rb")
+    network = pickle.load(pickle_in)
+    pickle_in.close()
+    network.predict(link)
+
+
+arguments = parser.parse_args()
+if arguments.hashtag != None and arguments.number_of_images != None:
+    get_data_from_instagram(arguments.hashtag, arguments.number_of_images)
+elif arguments.hashtags != None and len(arguments.hashtags) > 1:
+    make_neural_network(arguments.hashtags)
+elif arguments.link != None:
+    predict(arguments.link)
+elif arguments.test == True:
+    test_network()
--- neural_network.py
+++ neural_network.py
@@ -42,75 +42,78 @@ class neural_network:
         y = to_categorical(self.targets, len(self.hashtags))
 
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            X, y, test_size=0.33)
+            X, y, test_size=0.33
+        )
 
-        print("66% of the data {} and 33% of the data {}".format(
-            len(self.X_train), len(self.X_test)))
+        print(
+            "66% of the data {} and 33% of the data {}".format(
+                len(self.X_train), len(self.X_test)
+            )
+        )
 
     def make_model(self):
 
         self.get_images_from_csv()
 
         model = models.Sequential()
-        model.add(layers.Conv2D(
-            32, (3, 3), activation='relu', input_shape=(96, 96, 3)))
+        model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(96, 96, 3)))
         # Max Pooling to reduce the spatial dimensions of the output volume. pool_size: integer or tuple of 2 integers, factors by which to downscale (vertical, horizontal)
         model.add(layers.MaxPool2D((2, 2)))
         # does not need input_shape, since it gets it from previous layer
-        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Conv2D(64, (3, 3), activation="relu"))
         model.add(layers.MaxPool2D((2, 2)))
-        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Conv2D(64, (3, 3), activation="relu"))
         # rewrite tensor to single vector of values
         model.add(layers.Flatten())
-        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(64, activation="relu"))
         # softmax is good for output layer because Softmax outputs probabilities range. The range will 0 to 1, and the sum of all the probabilities will be equal to one. If the softmax function used for multi-classification model it returns the probabilities of each class and the target class will have the high probability.
-        model.add(layers.Dense(len(self.hashtags), activation='softmax'))
+        model.add(layers.Dense(len(self.hashtags), activation="softmax"))
 
-        model.compile(loss='categorical_crossentropy',  # loss is how to meassure how wrong the model is on its predictions
-                      optimizer='adam',  # rmsprop
-                      # what do we care about in our model
-                      metrics=['accuracy'],
-                      )
+        model.compile(
+            loss="categorical_crossentropy",  # loss is how to meassure how wrong the model is on its predictions
+            optimizer="adam",  # rmsprop
+            # what do we care about in our model
+            metrics=["accuracy"],
+        )
 
         self.model = model
 
     def train_network(self):
-        self.model.fit(self.X_train,
-                       self.y_train,
-                       epochs=5,
-                       verbose=True,
-                       batch_size=32,
-                       validation_split=0.1)  # checking periodically how well we are doing
+        self.model.fit(
+            self.X_train,
+            self.y_train,
+            epochs=5,
+            verbose=True,
+            batch_size=32,
+            validation_split=0.1,
+        )  # checking periodically how well we are doing
 
     def test_network(self):
         results = self.model.evaluate(self.X_test, self.y_test)
-        print('test loss, test acc:', results)
-    
+        print("test loss, test acc:", results)
+
     def predict(self, image_link):
         image = io.imread(image_link)
         array_image = resize(image, (96, 96, 3))
         image_list = np.array([array_image])
-        
-        
+
         tags = ""
         for hashtag in self.hashtags:
-            tags += ("{} ".format(hashtag))
-        
+            tags += "{} ".format(hashtag)
+
         print("\nOkay, let me check if thats an image of a: {}".format(tags))
 
         predicted = self.model.predict_classes(image_list)
 
-        print("\nThat sure looks like a {}".format(self.target_translator[predicted[0]]))
-        
-    
+        print(
+            "\nThat sure looks like a {}".format(self.target_translator[predicted[0]])
+        )
+
     def pickle_network(self):
-        pickle_out = open("network.pickle", "wb")
-        pickle.dump(self.model, pickle_out)
+        pickle_out = open("network.picked", "wb")
+        pickle.dump(self, pickle_out)
         pickle_out.close()
-    
+
     def get_pickled_network(self):
         pickle_in = open("network.pickle", "rb")
-        self.model = pickle.load(pickle_in)
-
-
-
+        self = pickle.load(pickle_in)
