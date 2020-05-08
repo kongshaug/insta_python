b17b0b7eba1f1ea6735b034ca42baa37581ceb9a
Amalie Landt <malie@hotmail.dk>
2020-05-08 14:25:14 +0200

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from insta_bot import instagram_bot
 from neural_network import neural_network
 
-cat_bot = instagram_bot("cat")
-dog_bot = instagram_bot("dog")
-tree_bot = instagram_bot("tree")
+# cat_bot = instagram_bot("cat")
+# dog_bot = instagram_bot("dog")
+# tree_bot = instagram_bot("tree")
 
-cat_bot.get_images(1500)
-dog_bot.get_images(1500)
-tree_bot.get_images(1500)
+# cat_bot.get_images(900)
+# dog_bot.get_images(900)
+# tree_bot.get_images(900)
 
 
-#network = neural_network(["cat", "dog", "tree"])
-# network.make_model()
-# network.train_network()
+network = neural_network(["cat", "dog", "tree"])
+network.make_model()
+network.train_network()
 # network.test_network()
